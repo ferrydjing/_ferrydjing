a8771c247818c4169b1c7cef0e87a89ac3a91019
dan ferry <danferry@dandeMacBook-Pro.local>
2020-08-05 17:04:42 +0800

    0.0.5.1596618282825

--- src/toPercent/index.js
+++ src/toPercent/index.js
@@ -1,5 +1,6 @@
 import calc from 'js-calculation'
 import { toCutDecimals } from '../toCutDecimals'
+
 export const toPercent = (point) => {
   if (typeof point === 'string' && !Number(point)) return point
   if (point === 0) return 0
